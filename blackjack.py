f962c7aecf3476f5fe3029a44a45737a1dc95d89
Soruso <burov.1990@gmail.com>
2022-05-13 12:12:01 +0300

    beta 0.2

--- blackjack.py
+++ blackjack.py
@@ -1,17 +1,28 @@
+from tkinter import *
 import random
-ruka = 0
-dealer = 0
-rating = 0
-while ruka <= 17:
-    ruka += random.randrange(1, 7, 1)
-    dealer += random.randrange(1, 7, 1)
-if ruka < 22 and dealer > 21 or dealer < ruka:
-    print("You win!")
-    rating += 1
-elif ruka == dealer:
-    print("Draw!")
-else:
-    print("You lose!")
-    rating -= 1
-print(ruka, dealer, rating)
-print("Зафиксировано")
+root = Tk()
+root.title("BlacJack")
+root.geometry("300x250")
+ 
+def  igra():
+    ruka = 0
+    dealer = 0
+    rating = 0
+    while True:
+        while ruka <= 17:
+            ruka += random.randrange(1, 7, 1)
+            dealer += random.randrange(1, 7, 1)
+        if ruka < 22 and dealer > 21 or dealer < ruka:
+            print("Вы победили!")
+            rating += 1
+        elif ruka == dealer:
+            print("Ничья!")
+        else:
+            print("Вы проиграли!")
+            rating -= 1
+        print("Ваши очки", ruka, "Очки диллера", dealer, "Ваш рейтинг", rating)
+        break
+
+btn = Button(text="Играть", command=igra)
+btn.pack()
+root.mainloop()
