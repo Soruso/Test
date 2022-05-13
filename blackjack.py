0e73594c8d73d8710915c8571434006b93694138
Soruso <burov.1990@gmail.com>
2022-05-13 15:39:25 +0300

    beta 0.3

--- blackjack.py
+++ blackjack.py
@@ -8,21 +8,20 @@ def  igra():
     ruka = 0
     dealer = 0
     rating = 0
-    while True:
-        while ruka <= 17:
-            ruka += random.randrange(1, 7, 1)
-            dealer += random.randrange(1, 7, 1)
-        if ruka < 22 and dealer > 21 or dealer < ruka:
-            print("Вы победили!")
-            rating += 1
-        elif ruka == dealer:
-            print("Ничья!")
-        else:
-            print("Вы проиграли!")
-            rating -= 1
-        print("Ваши очки", ruka, "Очки диллера", dealer, "Ваш рейтинг", rating)
-        break
-
+    while ruka <= 17:
+        ruka += random.randrange(1, 7, 1)
+        dealer += random.randrange(1, 7, 1)
+    if ruka < 22 and dealer > 21 or dealer < ruka:
+        print("Вы победили!")
+        rating += 1
+    elif ruka == dealer:
+        print("Ничья!")
+    else:
+        print("Вы проиграли!")
+        rating -= 1
+        
+    print("Ваши очки", ruka, "Очки диллера", dealer, "Ваш рейтинг", rating)
+    
 btn = Button(text="Играть", command=igra)
 btn.pack()
 root.mainloop()
